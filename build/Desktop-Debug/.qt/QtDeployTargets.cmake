46ba10dce87e2e05c229e1de16f197458743d312
nico-bertoli <nicobertoli.1999@gmail.com>
2024-12-26 01:56:35 +0100

    philosophers layout improvements

---
+++ build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_MultithreadingExperiments_FILE C:/Users/nico/Documents/DOC/99CosePesanti/Progetti/C++/QT/MultithreadingExperiments/build/Desktop-Debug/MultithreadingExperiments.exe)
+set(__QT_DEPLOY_TARGET_MultithreadingExperiments_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_MultithreadingExperiments_RUNTIME_DLLS C:/Qt/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
